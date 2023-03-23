37ab3467b6763d7d581cd77d76bcbd6b13445680
nitinmmmut <115034091+nitinmmmut@users.noreply.github.com>
2023-03-23 14:52:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 class Animal {
-  constructor(species) {
-    this._species = species;
-  }
+constructor(species) {
+this._species = species;
+}
 
-  get species() {
-    return this._species;
-  }
+get species() {
+return this._species;
+}
 
-  makeSound() {
-    console.log("The " + this.species + " makes a sound");
-  }
+makeSound() {
+console.log("The " + this.species + " makes a sound");
+}
 }
 
 class Cat extends Animal {
-  purr() {
-    console.log("purr");
-  }
+purr() {
+console.log("purr");
+}
 }
 
 class Dog extends Animal {
-  bark() {
-    console.log("woof");
-  }
+bark() {
+console.log("woof");
+}
 }
 
 // Do not change the code below this line
