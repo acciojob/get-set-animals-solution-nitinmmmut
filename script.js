47cfb9e735e4abaaa23da9c0033be827feddc725
nitinmmmut <115034091+nitinmmmut@users.noreply.github.com>
2023-04-03 10:11:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
 class Animal {
-constructor(species) {
-this._species = species;
+  constructor(species) {
+    this._species = species;
+  }
+  get species() {
+    return this._species;
+  }
+  makeSound() {
+    console.log("The " + this.species + " makes a sound");
+  }
 }
-
-get species() {
-return this._species;
-}
-
-makeSound() {
-console.log("The " + this.species + " makes a sound");
-}
-}
-
 class Cat extends Animal {
-purr() {
-console.log("purr");
+  purr() {
+    console.log("purr");
+  }
 }
-}
-
 class Dog extends Animal {
-bark() {
-console.log("woof");
+  bark() {
+    console.log("woof");
+  }
 }
-}
-
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
+
+function getElementsByAttribute(attribute, value) {
+  const elementsWithAttribute = [];
+  const allElements = document.getElementsByTagName("*");
+
+  for (let i = 0; i < allElements.length; i++) {
+    if (allElements[i].getAttribute(attribute) === value) {
+      elementsWithAttribute.push(allElements[i]);
+    }
+  }
+
+  return elementsWithAttribute;
+}
+ const elementsWithBarBaz = getElementsByAttribute("data-bar", "baz");
+    console.log(elementsWithBarBaz);
